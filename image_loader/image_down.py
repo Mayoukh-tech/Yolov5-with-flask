b7504ed085649f816de4e59a9e387bed29474b2e
techyfreek <114090817+Mayoukh-tech@users.noreply.github.com>
2023-04-18 20:11:05 +0530

    Update image_down.py

--- image_loader/image_down.py
+++ image_loader/image_down.py
@@ -1,5 +1,5 @@
 from simple_image_download import simple_image_download as simp
 response = simp.Downloader
-keyword = ["lion"]
+keyword = ["lion"] #here you replace the instance with your required instance
 for i in keyword:
-    response().download(i , 10)
+    response().download(i , 10) # here 10 denotes the number of images to be loaded
